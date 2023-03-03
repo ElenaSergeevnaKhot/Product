8966da69a3366737029e392e9a86954773d437e8
Elena <Elenakhot@mail.ru>
2023-03-03 18:59:22 +0300

    commit one

--- src/test/java/ProductManager/RepositoryTest.java
+++ src/test/java/ProductManager/RepositoryTest.java
@@ -20,4 +20,19 @@ public class RepositoryTest {
 
         Assertions.assertArrayEquals(expected, actual);
     }
+
+    @Test
+    public void testRemoveWhenProductNotExist() {
+        Repository repo = new Repository();
+        Book book1 = new Book(1, "Lord of the ring I", 100, "Tolkien");
+        Book book2 = new Book(2, "Hobbit", 200, "Tolkien");
+        Book book3 = new Book(3, "Lord of the ring III", 300, "Tolkien");
+
+        repo.add(book1);
+        repo.add(book2);
+        repo.add(book3);
+        Assertions.assertThrows(NotFoundException.class,
+                () -> repo.removeById(4)
+        );
+    }
 }
